e61c42cc16a7e77c2fdb5e492f43469764413a2c
Enry <christantoenry@gmail.com>
2018-09-17 15:06:28 +0700

    Importing Project

---
+++ ExcelImportExport/Models/ExcelUpload.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ExcelImportExport.Models
+{
+    [Table("Student")]
+    public class ExcelUpload
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudenteName { get; set; }
+    }
+}
